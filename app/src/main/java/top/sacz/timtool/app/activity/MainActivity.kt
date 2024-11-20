2b74ce5540c55a3c6c8fb98de4c67b73e595822b
suzhelan <suzhelan@gmail.com>
2024-11-20 21:29:28 +0800

    test:add dialogx show test

--- app/src/main/java/top/sacz/timtool/app/activity/MainActivity.kt
+++ app/src/main/java/top/sacz/timtool/app/activity/MainActivity.kt
@@ -5,12 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kongzue.dialogx.dialogs.MessageDialog
 import top.sacz.timtool.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -18,5 +18,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        MessageDialog.show("标题", "正文内容", "确定");
     }
 }
---
+++ app/src/main/java/top/sacz/timtool/util/FixContextUtil.java
@@ -0,0 +1,71 @@
+package top.sacz.timtool.util;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.view.LayoutInflater;
+
+
+public class FixContextUtil {
+    //获得注入了ClassLoader的Context,使其能加载模块中的类
+    public static Context getFixContext(Context context) {
+        return new FixContext(context);
+    }
+
+    //获得注入了ClassLoader的LayoutInflater,使其能加载模块中的类界面
+    public static LayoutInflater getContextInflater(Context context) {
+        return LayoutInflater.from(context).cloneInContext(getFixContext(context));
+    }
+
+    public static class FixResClassLoader extends ClassLoader {
+        protected FixResClassLoader(ClassLoader parent) {
+            super(parent);
+        }
+
+        @Override
+        public Class<?> loadClass(String name) throws ClassNotFoundException {
+            try {
+                if (!name.startsWith("com.airbnb.lottie")) {
+                    Class<?> clz = super.loadClass(name);
+                    if (clz != null) return clz;
+                }
+            } catch (Throwable e) {
+
+            }
+            return findClass(name);
+        }
+
+        @Override
+        protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            try {
+                if (!name.startsWith("com.airbnb.lottie")) {
+                    Class<?> clz = super.loadClass(name, resolve);
+                    if (clz != null) return clz;
+                }
+            } catch (Throwable e) {
+
+            }
+            return findClass(name);
+
+        }
+
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            return FixContextUtil.class.getClassLoader().loadClass(name);
+        }
+    }
+
+    public static class FixContext extends ContextWrapper {
+        private final ClassLoader mFixLoader;
+
+        public FixContext(Context base) {
+            super(base);
+            mFixLoader = new FixResClassLoader(base.getClassLoader());
+        }
+
+        @Override
+        public ClassLoader getClassLoader() {
+            if (mFixLoader != null) return mFixLoader;
+            return super.getClassLoader();
+        }
+    }
+}
