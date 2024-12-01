59c5a87fda44912bc0ef71174219c70e838dc8e8
suzhelan <suzhelan@gmail.com>
2024-12-01 20:05:26 +0800

    refactor: download emotion

--- app/src/main/java/top/sacz/timtool/hook/item/chat/DownloadEmotion.kt
+++ app/src/main/java/top/sacz/timtool/hook/item/chat/DownloadEmotion.kt
@@ -4,6 +4,7 @@ import top.sacz.timtool.hook.base.BaseSwitchFunctionHookItem
 import top.sacz.timtool.hook.core.annotation.HookItem
 import top.sacz.xphelper.reflect.MethodUtils
 
+
 @HookItem("辅助功能/表情与图片/禁用表情下载限制")
 class DownloadEmotion : BaseSwitchFunctionHookItem() {
     override fun getTip(): String {
@@ -11,15 +12,19 @@ class DownloadEmotion : BaseSwitchFunctionHookItem() {
     }
 
     override fun loadHook(loader: ClassLoader) {
-        val emotionDownloadDisableMethod =
-            MethodUtils.create("com.tencent.mobileqq.emotionintegrate.m")
-                .methodName("c")
-                .returnType(Boolean::class.javaPrimitiveType)
+        //有很多限制方法都走的这个接口
+        val isSwitchOn =
+            MethodUtils.create("com.tencent.mobileqq.activity.api.impl.UnitedConfigImpl")
+                .methodName("isSwitchOn")
+                .params(String::class.java, Boolean::class.javaObjectType)
+                .returnType(Boolean::class.javaObjectType)
                 .first()
-        hookBefore(emotionDownloadDisableMethod) { param ->
-            param.result = false
+        hookBefore(isSwitchOn) { param ->
+            val cmd = param.args[0] as String
+            if (cmd == "emotion_download_disable_8980_887036489") {
+                param.result = false
+            }
         }
-
     }
 
 }
