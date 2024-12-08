4aa80837ef4ee02bdb3402249c08e966c1feb352
suzhelan <suzhelan@gmail.com>
2024-12-08 18:55:11 +0800

    feat:In Friends, you can use file upload to rename

--- app/src/main/java/top/sacz/timtool/hook/item/chat/FileUploadRename.kt
+++ app/src/main/java/top/sacz/timtool/hook/item/chat/FileUploadRename.kt
@@ -6,7 +6,6 @@ import top.sacz.timtool.hook.HookEnv
 import top.sacz.timtool.hook.base.BaseSwitchFunctionHookItem
 import top.sacz.timtool.hook.core.annotation.HookItem
 import top.sacz.xphelper.reflect.FieldUtils
-import top.sacz.xphelper.reflect.Ignore
 import top.sacz.xphelper.reflect.MethodUtils
 import java.lang.reflect.Method
 
@@ -15,7 +14,7 @@ import java.lang.reflect.Method
 class FileUploadRename : BaseSwitchFunctionHookItem() {
 
     override fun getTip(): String {
-        return "自动重命名base为包名或应用名 自动将.apk重命名成.APK 防止被QQ自动重命名成.apk.1"
+        return "自动重命名base为包名或应用名 自动将.apk重命名成.APK 防止被QQ自动重命名为.apk.1"
     }
 
     override fun isLoadedByDefault(): Boolean {
@@ -23,15 +22,41 @@ class FileUploadRename : BaseSwitchFunctionHookItem() {
     }
 
     override fun loadHook(loader: ClassLoader) {
-        //TODO 私聊还没做好 等待完善
+
+        //私聊
         val friendMethod =
-            MethodUtils.create("com.tencent.mobileqq.filemanager.nt.NTC2CFileTransferMgr")
-                .returnType(Void.TYPE)
+            MethodUtils.create("com.tencent.mobileqq.filemanager.nt.NTFileManageBridger")
                 .params(
                     loader.loadClass("com.tencent.mobileqq.filemanager.data.FileManagerEntity"),
-                    Ignore::class.java
+                    Runnable::class.java,
+                    String::class.java,
+                    String::class.java
                 )
                 .first()
+        hookAfter(friendMethod, { param ->
+            val fileManagerEntity = param.args[0]
+            val fileName: String =
+                FieldUtils.getField(fileManagerEntity, "fileName", String::class.java)
+            val localFile: String =
+                FieldUtils.getField(fileManagerEntity, "strFilePath", String::class.java)
+            val meetHitConditions = meetHitConditions(fileName, localFile)
+            if (meetHitConditions) {
+                FieldUtils.setField(
+                    fileManagerEntity,
+                    "fileName",
+                    getFormattedFileNameByPath(localFile)
+                )
+            }
+        }, 25)
+        //第二次挂钩 将.apk替换成.APK
+        hookAfter(friendMethod, { param ->
+            val fileManagerEntity = param.args[0]
+            val fileName: String =
+                FieldUtils.getField(fileManagerEntity, "fileName", String::class.java)
+            if (fileName.endsWith(".apk")) {
+                FieldUtils.setField(fileManagerEntity, "fileName", fileName.replace(".apk", ".APK"))
+            }
+        }, 20)
 
         //群组
         val troopMethod: Method =
@@ -42,24 +67,6 @@ class FileUploadRename : BaseSwitchFunctionHookItem() {
                     loader.loadClass("com.tencent.mobileqq.troop.utils.TroopFileTransferManager\$Item")
                 )
                 .first()
-
-        //私聊
-        hookBefore(friendMethod, { param ->
-            val fileManagerEntity = param.args[0]
-            val fileName: String =
-                FieldUtils.getField(fileManagerEntity, "fileName", String::class.java)
-            val localFile: String =
-                FieldUtils.getField(fileManagerEntity, "strFilePath", String::class.java)
-            if (meetHitConditions(fileName, localFile)) {
-                FieldUtils.setField(
-                    fileManagerEntity,
-                    "fileName",
-                    getFormattedFileNameByPath(localFile)
-                )
-            }
-        }, 25)
-
-
         hookBefore(troopMethod, { param ->
             val item = param.args[1]
             val fileName: String = FieldUtils.getField(item, "FileName", String::class.java)
@@ -68,18 +75,6 @@ class FileUploadRename : BaseSwitchFunctionHookItem() {
                 FieldUtils.setField(item, "FileName", getFormattedFileNameByPath(localFile))
             }
         }, 25)
-
-
-        //第二次挂钩 将.apk替换成.APK
-        hookBefore(friendMethod, { param ->
-            val fileManagerEntity = param.args[0]
-            val fileName: String =
-                FieldUtils.getField(fileManagerEntity, "fileName", String::class.java)
-            if (fileName.endsWith(".apk")) {
-                FieldUtils.setField(fileManagerEntity, "fileName", fileName.replace(".apk", ".APK"))
-            }
-        }, 20)
-
         hookBefore(troopMethod, { param ->
             val item = param.args[1]
             val fileName: String = FieldUtils.getField(item, "FileName", String::class.java)
@@ -107,8 +102,12 @@ class FileUploadRename : BaseSwitchFunctionHookItem() {
             val applicationInfo = getAppInfoByFilePath(filePath) ?: return false
             //包名命中
             if (fileNameWithoutSuffix == applicationInfo.packageName) return true
-            //应用名命中
+            //应用主app context命中
             if (fileNameWithoutSuffix == applicationInfo.name) return true
+            //应用名命中
+            if (fileNameWithoutSuffix == applicationInfo.loadLabel(HookEnv.getHostAppContext().packageManager)
+                    .toString()
+            ) return true
         }
         return false
     }
