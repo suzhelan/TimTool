66817e69ccd21e5679458ac045220d162593a762
hd <hdshare@vip.qq.com>
2024-12-25 18:15:25 +0800

    feat: ForcePadMode
    Closes #19

---
+++ app/src/main/java/top/sacz/timtool/hook/item/experiment/ForcePadMode.kt
@@ -0,0 +1,19 @@
+package top.sacz.timtool.hook.item.experiment
+
+import top.sacz.timtool.hook.base.BaseSwitchFunctionHookItem
+import top.sacz.timtool.hook.core.annotation.HookItem
+import top.sacz.timtool.hook.util.getStaticFieldValue
+
+/**
+ * 思路 https://github.com/cinit/QAuxiliary -> cc.ioctl.hook.experimental.ForcePadMode
+ */
+@HookItem("辅助功能/实验性/强制平板模式")
+class ForcePadMode : BaseSwitchFunctionHookItem() {
+    override fun loadHook(loader: ClassLoader) {
+        val appSettingClass = loader.loadClass("com.tencent.common.config.AppSetting")
+        hookBefore(appSettingClass.getDeclaredMethod("f")) { param ->
+            val pad = appSettingClass.getStaticFieldValue<Int>("g")
+            param.result = pad
+        }
+    }
+}
