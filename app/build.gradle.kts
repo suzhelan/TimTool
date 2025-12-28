a7ec3c037549c80f2ec27c6992a65aa90c7cdb85
suzhelan <sacz@163.com>
2025-12-28 21:37:13 +0800

    chore:Keep dependencies fresh and ready to release new versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = BuildVersionConfig.applicationId
         minSdk = BuildVersionConfig.minSdk
         targetSdk = BuildVersionConfig.targetSdk
-        versionCode = 28
-        versionName = "2.8"
+        versionCode = 29
+        versionName = "2.9"
 
         ndk {
             //只支持arm64 v8a的lib so库,因为qq只支持arm64 v8a
